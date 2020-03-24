0ba24cf72db9aedcdc5f5eadef22bb9b8e4bbe9f
Ryan Duff <ryanduff613@gmail.com>
2020-03-24 14:52:09 -0700

    add classes for clickable light-dark theme
    Co-Author: Maria Lagunero <maria58@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+  $(".clickable").click(function() {
+  
+  });
+
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+
+
+
+
+});
