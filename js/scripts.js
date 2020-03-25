a287a1324d1a364c195e0f412c18d0929f84781a
Ryan Duff <ryanduff613@gmail.com>
2020-03-24 21:49:14 -0700

    delete commented out portions of styles, index and js files

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
 
-  $(".shown").click(function() {
-    $(".hidden").show();
-  });
-
   $("button#green").click(function() {
     $("body").removeClass();
     $("body").addClass("green-background");
@@ -23,10 +19,5 @@ $(document).ready(function() {
     $("body").removeClass("red-background green-background yellow-background");
     $("body").toggleClass("darkBackground");
   });
-
-  // $(".shown#clickable").click(function(){
-  //   $("body").removeClass();
-  //   $("body").addClass("lightBackground");
-  // });
-
+  
 });
